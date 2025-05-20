3c1264a1a9ee6343dc09282e0b4f68ddbae66b0a
Morgan N. Andayi <143075077+andayi-ck@users.noreply.github.com>
2025-05-20 14:24:55 +0300

    Update ProjectsStyles.js

--- portfolio_website-STARTER/src/components/Projects/ProjectsStyles.js
+++ portfolio_website-STARTER/src/components/Projects/ProjectsStyles.js
@@ -295,5 +295,12 @@ export const ExternalLinks = styled.a`
   text-decoration: none;
   cursor: pointer;
   box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+
+  &:hover {
+    background: ${(props) => {
+    // if (props.children === 'Source') return props.theme.colors.secondaryButtonHover || '#434190';
+    return props.theme.colors.primaryButtonHover || '#2980b9';
+  }};
+  
   
   
